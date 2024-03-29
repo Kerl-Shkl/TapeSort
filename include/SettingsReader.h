09019b0e0865d2de6529c9c4e9bdbb431a474c11
Kerl-Shkl <kirillshkalin@gmail.com>
2024-03-30 02:05:32 +0300

    Add settings reader

---
+++ include/SettingsReader.h
@@ -0,0 +1,32 @@
+#ifndef INCLUDE_SETTINGSREADER_H
+#define INCLUDE_SETTINGSREADER_H
+
+#include <chrono>
+#include <string>
+
+struct Settings
+{
+    using milliseconds = std::chrono::milliseconds;
+    uint32_t intermTapeCount = 1;
+    uint32_t memoryCapacity = 100;
+    milliseconds stepDelay = {};
+    milliseconds readDelay = {};
+    milliseconds writeDelay = {};
+    milliseconds rewindDelay = {};
+};
+
+class SettingsReader
+{
+public:
+    SettingsReader(const std::string& filename);
+    const Settings& getSettings();
+
+private:
+    void parseLine(const std::string& line);
+    void setSetting(const std::string& field, uint32_t value);
+
+    std::string filename;
+    Settings settings;
+};
+
+#endif  // end of include guard: INCLUDE_SETTINGSREADER_H
---
+++ src/SettingsReader.cpp
@@ -0,0 +1,75 @@
+#include "SettingsReader.h"
+#include <algorithm>
+#include <fstream>
+
+static void ltrim(std::string& s);
+static void rtrim(std::string& s);
+static void trim(std::string& s);
+
+const Settings& SettingsReader::getSettings()
+{
+    std::ifstream in(filename);
+    if (!in.is_open()) {
+        // TODO throw
+        return settings;
+    }
+    std::string line;
+    while (std::getline(in, line)) {
+        parseLine(line);
+    }
+    return settings;
+}
+
+void SettingsReader::parseLine(const std::string& line)
+{
+    const auto separator = line.find(':');
+    std::string field = line.substr(0, separator);
+    std::string valueStr = line.substr(separator, line.size());
+    trim(field);
+    trim(valueStr);
+    uint32_t value = std::stoul(valueStr);
+    setSetting(field, value);
+}
+
+void SettingsReader::setSetting(const std::string& field, uint32_t value)
+{
+    if (field == "interm_tape_count") {
+        settings.intermTapeCount = value;
+    }
+    else if (field == "memory_capacity") {
+        settings.memoryCapacity = value;
+    }
+    else if (field == "step_delay") {
+        settings.stepDelay = std::chrono::milliseconds(value);
+    }
+    else if (field == "read_delay") {
+        settings.readDelay = std::chrono::milliseconds(value);
+    }
+    else if (field == "write_delay") {
+        settings.writeDelay = std::chrono::milliseconds(value);
+    }
+    else if (field == "rewind_delay") {
+        settings.rewindDelay = std::chrono::milliseconds(value);
+    }
+}
+
+static void trim(std::string& s)
+{
+    rtrim(s);
+    ltrim(s);
+}
+
+static void ltrim(std::string& s)
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+                return !std::isspace(ch);
+            }));
+}
+
+static void rtrim(std::string& s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         [](unsigned char ch) { return !std::isspace(ch); })
+                .base(),
+            s.end());
+}
